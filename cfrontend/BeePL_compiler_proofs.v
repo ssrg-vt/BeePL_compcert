3b7bd00f458bb7827041ed1b2e71d4d2350c83f1
Swarn Priya <priya13swarn@gmail.com>
2025-01-24 10:53:12 -0500

    .

--- cfrontend/BeePL_compiler_proofs.v
+++ cfrontend/BeePL_compiler_proofs.v
@@ -2,132 +2,11 @@ Require Import String ZArith Coq.FSets.FMapAVL Coq.Structures.OrderedTypeEx FunI
 Require Import Coq.FSets.FSetProperties Coq.FSets.FMapFacts FMaps FSetAVL Nat PeanoNat Linking.
 Require Import Coq.Arith.EqNat Coq.ZArith.Int Integers AST Maps Linking Ctypes Smallstep SimplExpr.
 Require Import BeePL_aux BeePL_mem BeeTypes BeePL Csyntax Clight Globalenvs BeePL_Csyntax SimplExpr.
-Require Import compcert.common.Errors Initializersproof Cstrategy BeePL_auxlemmas lib.Coqlib Errors.
+Require Import Initializersproof Cstrategy BeePL_auxlemmas Coqlib Errors BeePL_typesystem_proofs.
 
 From mathcomp Require Import all_ssreflect. 
 
 (***** Correctness proof for the Csyntax generation from BeePL using BeePL compiler *****)
-
-(***** Specification for types *****) 
-Inductive rel_ptype : BeeTypes.primitive_type -> Ctypes.type -> Prop :=
-| rel_tunit : rel_ptype Tunit (Ctypes.Tvoid) (* Fix me *)
-| rel_tint : forall sz s a, 
-             rel_ptype (Tint sz s a) (Ctypes.Tint sz s a)
-| rel_tlong : forall s a, 
-              rel_ptype (Tlong s a) (Ctypes.Tlong s a). 
-
-Inductive rel_btype : BeeTypes.basic_type -> Ctypes.type -> Prop :=
-| rel_bt : forall p ct,
-           rel_ptype p ct ->
-           rel_btype (Bprim p) ct.
-
-Inductive rel_type : BeeTypes.type -> Ctypes.type -> Prop :=
-| rel_pt : forall p ct, 
-           rel_ptype p ct ->
-           rel_type (Ptype p) ct
-| rel_reftype : forall h bt ct a, 
-                rel_btype bt ct ->
-                rel_type (Reftype h bt a) (Tpointer ct a)
-| rel_ftype : forall ts ef t cts ct,
-              rel_types ts cts ->
-              rel_type t ct -> 
-              length ts = length (from_typelist cts) ->
-              rel_type (Ftype ts ef t) (Tfunction cts ct 
-                                        {| cc_vararg := Some (Z.of_nat(length(ts))); cc_unproto := false; cc_structret := false |}) 
-with rel_types : list BeeTypes.type -> Ctypes.typelist -> Prop :=
-| rel_tnil : rel_types nil Tnil
-| rel_tcons : forall bt bts ct cts,
-              rel_type bt ct ->
-              rel_types bts cts ->
-              rel_types (bt :: bts) (Tcons ct cts).
-
-Scheme rel_type_ind_mut := Induction for rel_type Sort Prop
-  with rel_typelist_ind_mut := Induction for rel_types Sort Prop.
-Combined Scheme rel_type_typelist_ind_mut from rel_type_ind_mut, rel_typelist_ind_mut.
-
-Section rel_type_ind.
-Context (Rts : list BeeTypes.type -> Ctypes.typelist -> Prop).
-Context (Rt : BeeTypes.type -> Ctypes.type -> Prop).
-Context (Rpt : BeeTypes.primitive_type -> Ctypes.type -> Prop).
-Context (Rbt : BeeTypes.basic_type -> Ctypes.type -> Prop).
-Context (Rtunit : Rpt Tunit (Ctypes.Tvoid)).
-Context (Rtint : forall sz s a, Rpt (Tint sz s a) (Ctypes.Tint sz s a)).
-Context (Rtlong : forall s a, Rpt (Tlong s a) (Ctypes.Tlong s a)).
-Context (Rbth : forall p ct, Rpt p ct -> Rbt (Bprim p) ct).
-Context (Rpth : forall p ct, Rpt p ct -> Rt (Ptype p) ct).
-Context (Rreft : forall h bt a ct, Rbt bt ct -> Rt (Reftype h bt a) (Tpointer ct a)). 
-Context (Rfunt : forall ts ef t cts ct, Rts ts cts -> Rt t ct -> 
-                 Rt (Ftype ts ef t) (Tfunction cts ct 
-                                     {| cc_vararg := Some (Z.of_nat(length(ts))); cc_unproto := false; cc_structret := false |})).
-Context (Rnil : Rts nil Tnil).
-Context (Rcons : forall t ct ts cts, Rt t ct -> Rts ts cts -> Rts (t :: ts) (Tcons ct cts)).
-
-Lemma rel_type_indP : 
-(forall t ct, rel_type t ct -> Rt t ct) /\
-(forall ts cts, rel_types ts cts -> Rts ts cts).
-Proof. 
-apply rel_type_typelist_ind_mut=> //=.
-+ move=> p ct /= hr. apply Rpth. case: p hr=> //=.
-  + case: ct=> //=.
-    + by move=> i s a hr;inversion hr.
-    + by move=> s a hr;inversion hr.
-    + by move=> f a hr; inversion hr.
-    + by move=> t a hr; inversion hr.
-    + by move=> t z a hr; inversion hr.
-    + by move=> ts t c hr; inversion hr.
-    + by move=> i a hr; inversion hr.
-    by move=> i a hr; inversion hr.
-  + move=> i s a hr. case: ct hr=> //=.
-    + by move=> hr; inversion hr.
-    + by move=> sz' s' a' hr; inversion hr; subst.
-    + by move=> s' a' hr; inversion hr; subst.
-    + by move=> f a' hr; inversion hr.
-    + by move=> t a' hr; inversion hr.
-    + by move=> t z a' hr; inversion hr.
-    + by move=> t t' c hr; inversion hr.
-    + by move=> i' a' hr; inversion hr.
-    by move=> i' a' hr; inversion hr.
-  + case: ct=> //=.
-    + by move=> s a hr; inversion hr.
-    + by move=> sz s a s' a' hr; inversion hr; subst.
-    + by move=> s a s' a' hr; inversion hr; subst.
-    + by move=> f a s a' hr; inversion hr; subst.
-    + by move=> t a s a' hr; inversion hr.
-    + by move=> t z a s a' hr; inversion hr.
-    + by move=> ts t c s a hr; inversion hr.
-    + by move=> i a s a' hr; inversion hr.
-    by move=> i a s a' hr; inversion hr.
-+ move=> h bt ct a hr; inversion hr; subst.
-  apply Rreft. apply Rbth. case: p hr H=> //=.
-  + case: ct=> //=.
-    + by move=> i s a' hr H; inversion H; subst.
-    + by move=> s a' hr H; inversion H; subst.
-    + by move=> f a' hr H; inversion H.
-    + by move=> t a' hr H; inversion H.
-    + by move=> t z a' hr H; inversion H.
-    + by move=> ts t c hr H; inversion H.
-    + by move=> i a' hr H; inversion H.
-    + by move=> i a' hr H; inversion H.
-    + by move=> u s a' hr H; inversion H.
-    by move=> s a' hr H; inversion H.
-+ move=> ts ef t cts ct hrs hts hr ht hd.
-  by move: (Rfunt ts ef t cts ct hts ht).
-move=> bt bts ct cts hr ht hrs hts. 
-by move: (Rcons bt ct bts cts ht hts).
-Qed. 
-
-End rel_type_ind.
-
-(***** Proof for correctness of type transformation *****)
-Lemma type_translated: 
-(forall bt ct, 
-transBeePL_type bt = OK ct ->
-rel_type bt ct) /\ 
-(forall bts cts, 
-transBeePL_types transBeePL_type bts = OK cts ->
-rel_types bts cts).
-Proof.
-Admitted.
 
 Section specifications.
 
@@ -202,12 +81,18 @@ Inductive sim_bexpr_cexpr : vmap -> BeePL.expr -> Csyntax.expr -> Prop :=
               sim_bexpr_cexpr le (BeePL.Hexpr h e t) (Eval (Values.Vundef) Tvoid).
  
 
-(* Complete me *)
-Inductive sim_expr_stmt : BeePL.expr -> Csyntax.statement -> Prop :=
+(* Complete me *)  
+Inductive sim_bexpr_cstmt : BeePL.expr -> Csyntax.statement -> Prop :=
 | sim_val_st : forall v t ct cv,
-              transBeePL_type t = OK ct ->
-              transBeePL_value_cvalue v = cv ->
-              sim_expr_stmt (BeePL.Val v t) (Csyntax.Sreturn (Some (Eval (transBeePL_value_cvalue v) ct))).
+               transBeePL_type t = OK ct ->
+               transBeePL_value_cvalue v = cv ->
+               sim_bexpr_cstmt (BeePL.Val v t) 
+                               (Csyntax.Sreturn (Some (Eval (transBeePL_value_cvalue v) ct))).
+(*| sim_valof_st : forall e t ct ce,
+                 transBeePL_type t = OK ct ->
+                 transBeePL_expr_expr e = OK ce ->*)
+
+
 
 Lemma tranBeePL_expr_expr_spec: forall vm e ce,
 transBeePL_expr_expr e = OK ce ->
@@ -224,7 +109,7 @@ Admitted.
 
 Lemma tranBeePL_expr_stmt_spec: forall e ce,
 transBeePL_expr_st e = OK ce ->
-sim_expr_stmt e ce.
+sim_bexpr_cstmt e ce.
 Proof.
 Admitted.
 
@@ -246,7 +131,7 @@ Inductive match_function : BeePL.function -> Csyntax.function -> Prop :=
   extract_vars_vinfos (BeePL.fn_vars bf) = unzip1 (Csyntax.fn_vars cf) ->
   transBeePL_types transBeePL_type (extract_types_vinfos (BeePL.fn_vars bf)) = 
   OK (to_typelist (unzip2 (Csyntax.fn_vars cf))) ->
-  sim_expr_stmt (BeePL.fn_body bf) (Csyntax.fn_body cf) ->
+  sim_bexpr_cstmt (BeePL.fn_body bf) (Csyntax.fn_body cf) ->
   match_function bf cf.
 
 Lemma tranBeePL_function_spec: forall bf cf,
@@ -673,6 +558,57 @@ move=> e m e' m' ce hr. induction hr; subst; rewrite /=.
   admit.
 (* unit *)
 admit.
-Admitted.   
+Admitted.  
+
+(*** Matching between continuations ***) 
+Inductive match_bcont_ccont : composite_env -> BeePL.cont -> Csem.cont -> Prop :=
+| match_Kstop : match_bcont_ccont bprog.(prog_comp_env) BeePL.Kstop Csem.Kstop
+| match_Kdo : forall bc cc,
+              match_bcont_ccont bprog.(prog_comp_env) bc cc ->
+              match_bcont_ccont bprog.(prog_comp_env) (BeePL.Kdo bc) (Csem.Kdo cc)
+| match_Klet : forall be cst cb cc, 
+               transBeePL_expr_st be = OK cst ->
+               sim_bexpr_cstmt be cst -> 
+               match_bcont_ccont bprog.(prog_comp_env) (Klet be cb) (Csem.Kseq cst cc)
+| match_Kcall : forall bf cf vm cenv BC CC bt ct bc cc,
+                (* add linking order ?? *)
+                transBeePL_function_function bf = OK cf ->
+                match_env benv cenv ->
+                match_bcont_ccont bprog.(prog_comp_env) bc cc ->
+                match_bcont_ccont bprog.(prog_comp_env) (BeePL.Kcall bf vm BC bt bc) 
+                                       (Csem.Kcall cf cenv CC ct cc)
+| match_Kcond : forall be1 be2 bc cst1 cst2 cc,
+                transBeePL_expr_st be1 = OK cst1 ->
+                transBeePL_expr_st be2 = OK cst2 ->
+                match_bcont_ccont bprog.(prog_comp_env) bc cc ->
+                match_bcont_ccont bprog.(prog_comp_env) (BeePL.Kcond be1 be2 bc)
+                                              (Csem.Kifthenelse cst1 cst2 cc).
+
+(*** Matching between states ***) 
+Inductive match_bstate_cstate : BeePL.state -> Csem.state -> Prop :=
+| match_exprstate : forall bf cf be ce bc cc m,
+                    transBeePL_function_function bf = OK cf ->
+                    transBeePL_expr_expr be = OK ce ->
+                    sim_bexpr_cexpr benv be ce ->
+                    match_env benv cenv ->
+                    match_bcont_ccont bprog.(prog_comp_env) bc cc ->
+                    match_bstate_cstate (BeePL.ExprState bf be bc benv m) 
+                                        (Csem.ExprState cf ce cc cenv m)
+| match_state : forall bf cf be cst bc cc m, (* Fix me *)
+                transBeePL_function_function bf = OK cf ->
+                transBeePL_expr_st be = OK cst ->
+                sim_bexpr_cstmt be cst ->
+                match_env benv cenv ->
+                match_bcont_ccont bprog.(prog_comp_env) bc cc ->
+                match_bstate_cstate (BeePL.ExprState bf be bc benv m) 
+                                    (Csem.State cf cst cc cenv m)
+| match_callstate : forall bfd bvs bc m cfd cvs cc,
+                    transBeePL_fundef_fundef bfd = OK cfd ->
+                    transBeePL_values_cvalues bvs = cvs ->
+                    match_bcont_ccont bprog.(prog_comp_env) bc cc ->
+                    match_bstate_cstate (BeePL.CallState bfd bvs bc m)
+                                        (Csem.Callstate cfd cvs cc m)
+| match_stuckstate : match_bstate_cstate (BeePL.StuckState) (Csem.Stuckstate).
+
   
 End semantic_preservation.
